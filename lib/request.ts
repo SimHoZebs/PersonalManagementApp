92b698819d4d74f76f05bdd769e9ad9ada1193c0
SimHoZebs <simhozebs@gmail.com>
2021-10-11 16:47:53 -0400

    fix 54: domains have no access to deployment

--- lib/request.ts
+++ lib/request.ts
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 const request = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL === "http://localhost:3000"
-    ? "http://localhost:3000"
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+  baseURL: process.env.URL
 })
 
 export default request
