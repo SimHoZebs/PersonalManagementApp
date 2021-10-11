f4ca565fcd0274f2a7cbe8ced2ee339181915e9e
SimHoZebs <simhozebs@gmail.com>
2021-10-10 21:42:47 -0400

    added page redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: process.env.NEXT_PUBLIC_VERCEL_URL,
+        permanent: true,
+      },
+    ]
+  }
 }
