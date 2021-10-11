59e30cad289125cf4282972e0a6d6a8e95b8e03f
SimHoZebs <simhozebs@gmail.com>
2021-10-11 16:46:49 -0400

    fix 54: domains have no access to deployment

--- lib/request.ts
+++ lib/request.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 const request = axios.create({
   baseURL: process.env.NEXT_PUBLIC_VERCEL_URL === "http://localhost:3000"
     ? "http://localhost:3000"
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+    : `https://${process.env.URL}`,
 })
 
 export default request
--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_VERCEL_URL === "http://localhost:3000"
-          ? "http://localhost:3000/api:path*"
-          : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/:path*`,
-      }
-    ]
-  }
 }
