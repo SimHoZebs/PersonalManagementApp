92fe6857d3cf55978efe0d797e5985ed420971f5
SimHoZebs <simhozebs@gmail.com>
2021-10-30 12:31:38 +0900

    initialProps for dashboard

--- pages/user/[userId]/index.tsx
+++ pages/user/[userId]/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { NextPageContext } from "next";
 import Head from "next/head";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import List from "../../../lib/components/List";
-import { useRouter } from "next/router";
 import isLoaded from "../../../lib/isLoaded";
 
 //API functions
@@ -14,82 +14,24 @@ import updateSelectedListId from "../../../lib/api/updateSelectedListId";
 
 //schema and interfaces
 import { UserSchema } from "../../../lib/schema/UserSchema";
-import { ListSchema } from "../../../lib/schema/ListSchema";
 import SideMenu from "../../../lib/components/SideMenu";
 import { Skeleton } from "@mui/material";
+
+interface Props {
+  user: UserSchema;
+}
 
 /**
  * displays user dashboard.
  *
  */
-export default function Dashboard() {
+export default function Dashboard(props: Props) {
   const [user, setUser] = useState<UserSchema | undefined>();
   const [currListName, setCurrListName] = useState<string | undefined>();
-  const router = useRouter();
 
   useEffect(() => {
-    /**
-     * Creates a list and adds it to a user.
-     * Sets that list as selected list.
-     * @returns userSchema; The user the temp defaults were applied.
-     * @returns string; if any error occurs.
-     */
-
-    async function addTempDefaults(userId: string) {
-      let createdList: ListSchema;
-      const createListRes = await createList(userId, "Welcome!");
-      if (typeof createListRes === "string") {
-        return createListRes;
-      }
-      createdList = createListRes;
-
-      const addListIdRes = await addListId(userId, createdList._id);
-      if (typeof addListIdRes === "string") {
-        return addListIdRes;
-      }
-
-      const updateSelectedListIdRes = await updateSelectedListId(
-        userId,
-        createListRes._id
-      );
-      return updateSelectedListIdRes;
-    }
-    /**
-     * Reads user data with given userId.
-     */
-    async function initUserPage() {
-      const userId = router.query.userId;
-      if (typeof userId !== "string") {
-        return;
-      }
-
-      const readUserRes = await readUser(userId);
-      if (typeof readUserRes === "string") {
-        console.log(readUserRes);
-        return;
-      }
-      if (readUserRes === null) {
-        console.log(`User with id ${userId} does not exist.`);
-        //route back to login screen?
-        return;
-      }
-
-      let user = readUserRes;
-
-      if (user.listIdArray.length === 0) {
-        const addTempDefaultsRes = await addTempDefaults(userId);
-        if (typeof addTempDefaultsRes === "string") {
-          console.log(addTempDefaultsRes);
-          return;
-        }
-        user = addTempDefaultsRes;
-      }
-
-      setUser(user);
-    }
-
-    initUserPage();
-  }, [router.query.userId]);
+    setUser(props.user);
+  }, []);
 
   return (
     <>
@@ -128,3 +70,70 @@ export default function Dashboard() {
     </>
   );
 }
+
+/**
+ * Creates a list and adds it to a user.
+ * Sets that list as selected list.
+ * @returns userSchema; The user the temp defaults were applied.
+ * @returns string; if any error occurs.
+ */
+
+Dashboard.getInitialProps = async (context: NextPageContext) => {
+  async function addTempDefaults(userId: string) {
+    const createListRes = await createList(userId, "Welcome!");
+    if (typeof createListRes === "string") {
+      return createListRes;
+    }
+    const createdList = createListRes;
+
+    const addListIdRes = await addListId(userId, createdList._id);
+    if (typeof addListIdRes === "string") {
+      return addListIdRes;
+    }
+
+    const updateSelectedListIdRes = await updateSelectedListId(
+      userId,
+      createListRes._id
+    );
+    return updateSelectedListIdRes;
+  }
+
+  /**
+   * Reads user data with given userId.
+   */
+  async function initUserPage() {
+    let user: UserSchema;
+    const userId = context.query.userId;
+    if (typeof userId !== "string") {
+      return;
+    }
+
+    const readUserRes = await readUser(userId);
+    if (typeof readUserRes === "string") {
+      console.log(readUserRes);
+      return;
+    }
+    if (readUserRes === null) {
+      console.log(`User with id ${userId} does not exist.`);
+      //route back to login screen?
+      return;
+    }
+
+    user = readUserRes;
+
+    if (user.listIdArray.length === 0) {
+      const addTempDefaultsRes = await addTempDefaults(userId);
+      if (typeof addTempDefaultsRes === "string") {
+        console.log(addTempDefaultsRes);
+        return;
+      }
+      user = addTempDefaultsRes;
+    }
+
+    return user;
+  }
+
+  const user = await initUserPage();
+
+  return { user };
+};
