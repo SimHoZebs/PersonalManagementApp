d5369ca4090faa8cbcf87d6305b715c3f3cf5cc3
SimHoZebs <simhozebs@gmail.com>
2021-12-14 13:03:37 -0500

    better looking buttons

--- lib/components/StatusButton.tsx
+++ lib/components/StatusButton.tsx
@@ -2,10 +2,10 @@ import { Icon } from "@iconify/react";
 
 const StatusButton = () => {
   return (
-    <div className="flex flex-col gap-1">
-      <Icon icon="akar-icons:chevron-up" className="h-4 w-4" />
+    <div className="flex flex-col gap-1 items-center">
+      <Icon icon="entypo:chevron-up" className="h-6 w-6" />
       <div className="h-4 w-4 bg-purple-400 rounded-sm"></div>
-      <Icon icon="akar-icons:chevron-down" className="h-4 w-4" />
+      <Icon icon="entypo:chevron-down" className="h-6 w-6" />
     </div>
   );
 };
