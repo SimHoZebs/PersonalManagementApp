f5c1fb7d6ef7b8f84844662cdb70e3cb0a906d42
SimHoZebs <simhozebs@gmail.com>
2021-12-17 16:53:55 -0500

    exclude ts files

--- windi.config.ts
+++ windi.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'windicss/helpers';
 
 export default defineConfig({
   extract: {
-    include: ['**/*.{tsx,ts}'],
+    include: ['**/*.{tsx}'],
     exclude: ['node_modules', '.next', '.vscode', 'lib/api', 'lib/schema', 'lib/functions']
   },
   theme: {
